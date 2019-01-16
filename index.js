1ee58188d4e7ba0fc74496a50e4a00703bc7195e
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-01-16 22:07:54 +0800

    Changes: 1.	Created a rates.json file to move the rates object out of the main program 	and into the fs for data persistence after modification across multiple runs.
    2.	Created a program_data.json file to store all the program data like the
    	precision level and more.
    
    3.	Working on a new function to set a new Base/Standard currency.

--- index.js
+++ index.js
@@ -8,25 +8,42 @@
 		The rates will be either stored in a JSON file, or read from real time stats
 */
 
+// Dependencies
+const rates = require('./rates.json'); // Import and parse JSON file
+const fs = require('fs'); // Used to read and write to the fs to store the rates data.
+
 // Testing util
 const log = (str) => console.log(str);
 
 // The currency that all other currencies are measured/valued against.
-var std_currency = 'USD'; // also known as the std. value
+var std_currency = 'USD'; // also known as the std. value or the base currency for comparision
 // Function to set the currency to be used as the std. of conversion.
-const set_std_currency = (cur) => rates[cur] ? (std_currency = cur) : new Error('ERROR: Currency does not exist');
-// How precise in Decimal Points.
-var precision = 3;
-// Function to set precision level
-const set_precision = (dpts) => precision = dpts; // Set to the input decimal points
+// const set_std_currency = (cur) => rates[cur] ? (std_currency = cur) : new Error('ERROR: Currency does not exist');
+const set_std_currency = (cur) => {
+	function convert_rates() {
+		rates.forEach((rate) => {
+			
+		})
 
-const rates = {
-	USD: 1,
-	SGD: 1.36,
-	GBP: 0.78,
-	AUD: 1.39
+		// How to make multiple properties in JS obj point to the same value.
+
+		// Last step is to set the currency
+		std_currency = cur;
+		// Also after converting the whole rates file, save it back to the rates file.
+		fs.open(`rates_${cur}.json`, 'w+', (err, fd) => {
+			if (err)
+				return console.error(err);
+			// fs.write(fd, JSON.stringify(rates), (err, written))
+		});
+	}
+
+	// rates[cur] ? convert_rates() : new Error('ERROR: Currency does not exist');
+	rates[cur] ? convert_rates() : new Error('ERROR: Currency does not exist');
 };
 
+const { precision } = require('./program_data.json');
+// Function to set precision level
+const set_precision = (dpts) => precision = dpts; // Set to the input decimal points
 // Function to convert currency to uppercase, which is used throught the whole program.
 const getCurrency = (cur) => cur.trim().toUpperCase();
 // Convert the given currency into the standard measurement currency
