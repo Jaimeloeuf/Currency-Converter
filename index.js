a10e81eb9bb8efd589f777dbde19f67eb070a41c
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-01-16 23:51:38 +0800

    Changes: 1.	Added methods to loop through the rates to convert between different 	std. or different base currencies.
    2.	Worked on method to save and persist the persistence variable to file.

--- index.js
+++ index.js
@@ -6,11 +6,21 @@
 	@Program Flow:
 	The rates file will be read in from the file system at program startup
 		The rates will be either stored in a JSON file, or read from real time stats
+
+	
+	@Features to do:
+	- Implement a history feature, where you can recall your last few conversions
+	- Make it into a CLI tool
 */
 
 // Dependencies
 const rates = require('./rates.json'); // Import and parse JSON file
 const fs = require('fs'); // Used to read and write to the fs to store the rates data.
+/*	@Destructure out:
+	- Precision variable: How precise the output will be in Decimal Points
+*/
+const { precision } = require('./program_data.json');
+
 
 // Testing util
 const log = (str) => console.log(str);
@@ -22,8 +32,23 @@ var std_currency = 'USD'; // also known as the std. value or the base currency f
 const set_std_currency = (cur) => {
 	function convert_rates() {
 		rates.forEach((rate) => {
-			
+
 		})
+
+		for (let key in rates) {
+			key
+		}
+
+		Object.keys(rates).forEach((key) => {
+
+		})
+
+		/*	How do I convert the values to the new std base?
+			- Find the rate of the currency to convert to from the obj.
+			- Keep track of the new base_currency and the old base_currency
+			use the convert function first, with a value of 1
+			convert(Currency, GBP, 1)
+		*/
 
 		// How to make multiple properties in JS obj point to the same value.
 
@@ -41,9 +66,15 @@ const set_std_currency = (cur) => {
 	rates[cur] ? convert_rates() : new Error('ERROR: Currency does not exist');
 };
 
-const { precision } = require('./program_data.json');
 // Function to set precision level
-const set_precision = (dpts) => precision = dpts; // Set to the input decimal points
+// const set_precision = (dpts) => precision = dpts; // Set to the input decimal points
+function set_precision(dpts) {
+	// Set the precision level to the input decimal points
+	precision = dpts;
+	// Persist this data to the configuration/program-data file.
+
+}
+
 // Function to convert currency to uppercase, which is used throught the whole program.
 const getCurrency = (cur) => cur.trim().toUpperCase();
 // Convert the given currency into the standard measurement currency
