d890d04254b4420226ab88ae35859709b6aba45e
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-03-14 22:30:00 +0800

    New unit tests directory: 1.  Created a new directory at the repo's root directory to store all the unit     tests for the modules inside the lib directory.
    2.  Alongside this new directory, a unit test for the index module has also been
        created, called convert, to test all the convertion methods. Currently only
        contains basic scaffolding code.

---
+++ unit tests/convert.js
@@ -0,0 +1,11 @@
+'use strict'; // Enforce use of strict version of JavaScript
+
+/*	@Doc
+	Unit test module for the convert modules
+
+	@Todo:
+	- 
+*/
+
+const convert = require('../lib/index');
+const assert = require('assert');
